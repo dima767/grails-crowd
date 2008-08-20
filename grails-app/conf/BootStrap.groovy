f82ed6bef4e726294a68406493c50383fa739ce0
Dmitriy Kopylenko <dima767@gmail.com>
2008-08-20 16:54:50 -0400

    Cleaned up Bootstrap.groovy - deleted original init code that was used in the early stages of development

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,31 +2,7 @@ import grailscrowd.core.*
 
 class BootStrap {
 
-    ProjectService projectService
-
     def init = {servletContext ->
-        //Here just for testing on the initial stages of development
-        /*for (i in 1..20) {
-            new Member(name: "name$i", email: "name$i@gmail.com", password: "passwd$i", displayName: "Name $i",
-            mailbox: new Mailbox()).save(flush: true)
-        }
-        def creatorMember = Member.findByName("name1");
-        def project = projectService.createProject(uri: 'http://grailscrowd.com',
-                name: 'GrailsCrowd',
-                description: 'Test app',
-                primaryLocation: 'USA',
-                architectureDescription: 'grails/mvc', tagTokens: 'grails,groovy,java'.encodeAsUniqueList(),
-                creatorMember)
-        if(project.hasErrors()) {
-            log.info(project.errors)
-        }
-        projectService.createProject(uri: 'http://example1.com',
-                name: 'ExampleOne',
-                description: 'Example 1 project',
-                primaryLocation: 'USA',
-                architectureDescription: 'Rich UI (Flex) based application with Grails backend. Has Atom feeds and number of plugins',
-                tagTokens: 'grails,flex,social network'.encodeAsUniqueList(),
-                creatorMember)*/
     }
 
     def destroy = {
